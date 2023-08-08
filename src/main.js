a48f7d8bd23a72a5cf32061c90dbc046874c094a
gitraun <raunaksrivastava2000@gmail.com>
2023-08-08 16:56:54 +0530

    updated

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from "./src/components/navbar.js";
-import Banner from "./src/components/banner.js";
-import Footer from "./src/components/footer.js";
+import Navbar from "./components/navbar.js";
+import Banner from "./components/banner.js";
+import Footer from "./components/footer.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
